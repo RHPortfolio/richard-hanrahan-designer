7312f27da88f32412cd493919211dafe33b8c261
Richard Hanrahan <fahitch@gmail.com>
2022-09-23 21:23:56 +0100

    Updates useWindowsDimension hook for SSR compatibility

--- src/hooks/useWindowsDimension.js
+++ src/hooks/useWindowsDimension.js
@@ -1,26 +1,53 @@
  // Code from https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs/36862446#36862446
 
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+//
+// function getWindowDimensions() {
+//   const { innerWidth: width, innerHeight: height } = window;
+//   return {
+//     width,
+//     height
+//   };
+// }
+//
+// export default function useWindowDimensions() {
+//   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+//
+//   useEffect(() => {
+//     function handleResize() {
+//       setWindowDimensions(getWindowDimensions());
+//     }
+//
+//     window.addEventListener('resize', handleResize);
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
+//
+//   return windowDimensions;
+// }
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
+// From here - https://gist.github.com/joshcawthorne/0a518b164658510f4eed74d0c4e8d003
+import { useState, useEffect } from "react";
+import { window } from "browser-monads"; //npm i browser-monads
 
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+function useWindowsDimension() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return windowDimensions;
+  return windowSize;
 }
+
+export default useWindowsDimension;
