ec6c8ff6a74d4680d07eb0e0780db225a18e1555
Richard Hanrahan <fahitch@gmail.com>
2022-09-25 23:09:27 +0100

    Update image component to use GatsbyImage

--- src/components/image-wall-image.js
+++ src/components/image-wall-image.js
@@ -1,9 +1,10 @@
 import React, { useState} from "react"
 import * as styles from "@components/image-wall.css"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { Link } from 'gatsby';
 
-export default function ImageWallImage({ title, src, category, index, highlighted, expandedHeight, column, url, hasPage, isMobile}) {
+export default function ImageWallImage({ title, src, category, index, highlighted, expandedHeight, column, url, hasPage, isMobile, displayImage}) {
+
 
   const [ expanded, setExpanded ] = useState(false);
   const isHighlighted = highlightedCheck(highlighted);
@@ -63,23 +64,54 @@ export default function ImageWallImage({ title, src, category, index, highlighte
      return `&w=${width}&h=${isHighlighted ? 2 * height : height}`
    }
 
+  function determineImage(){
+    // if (isMobile && isHighlighted){
+    //   return displayImage.mobileHighlighted
+    // }
+    //
+    // if (isMobile ){
+    //   return displayImage.mobile
+    // }
+    //
+    // if(isHighlighted) {
+    //   return displayImage.highlighted
+    // }
+    //
+    // return displayImage.default
+    return displayImage.image
+  }
+
   const params = "?fit=crop&q=85&auto=format&crop=focalpoint";
-  const children = (<img
-    key={index}
+  const children = (<GatsbyImage
+    image={determineImage()}
+    alt={title + ", created by Richard Hanrahan"}
     style={expanded ?
       { transition: "2s", width: "100%", height: expandedHeight, maxWidth: "inherit" }
       : { transition: "0.5s"}}
-    className={
-      (isHighlighted ? styles.highlightedImage : styles.image)
-      + ` column${column} `
-      + (isMobile ? styles.mobileImageMargin : styles.imageMargin)}
-    srcSet={
-      src + params + addDimensions(isMobile, isHighlighted) + "&dpr=1 1x," +
-      src + params + addDimensions(isMobile, isHighlighted, 2) + "&dpr=2 2x," +
-      src + params + addDimensions(isMobile, isHighlighted, 2.5) + "&dpr=3 3x," }
-    src={ src + params + addDimensions(isMobile, isHighlighted)}
-    title={title}
-    alt={title + ", created by Richard Hanrahan"} />)
+      className={
+        (isHighlighted ? styles.highlightedImage : styles.image)
+        + ` column${column} `
+        + (isMobile ? styles.mobileImageMargin : styles.imageMargin)}
+        title={title}
+
+    />)
+    // Ready to delete
+  // const oldChildren = (<img
+  //   key={index}
+  //   style={expanded ?
+  //     { transition: "2s", width: "100%", height: expandedHeight, maxWidth: "inherit" }
+  //     : { transition: "0.5s"}}
+  //   className={
+  //     (isHighlighted ? styles.highlightedImage : styles.image)
+  //     + ` column${column} `
+  //     + (isMobile ? styles.mobileImageMargin : styles.imageMargin)}
+  //   srcSet={
+  //     src + params + addDimensions(isMobile, isHighlighted) + "&dpr=1 1x," +
+  //     src + params + addDimensions(isMobile, isHighlighted, 2) + "&dpr=2 2x," +
+  //     src + params + addDimensions(isMobile, isHighlighted, 2.5) + "&dpr=3 3x," }
+  //   src={ src + params + addDimensions(isMobile, isHighlighted)}
+  //   title={title}
+  //   alt={title + ", created by Richard Hanrahan"} />)
 
   return (
     <>
